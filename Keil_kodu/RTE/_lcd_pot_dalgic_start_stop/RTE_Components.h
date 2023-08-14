7066961e25ff0e80cdff95f279a63e812a246e07
Mustafa Vural <vuralmustafa474@gmail.com>
2023-08-14 19:16:01 +0300

    lcd_acd_proje
    Bir lcd yardımıyla potansiyometre değeri ve acd değeri okuyan proje tasarladım. Start ve stop butonlarıyla sistemin açılıp kapanması ve led ile bu durumun kolayca anlaşılmasını istedim

---
+++ Keil_kodu/RTE/_lcd_pot_dalgic_start_stop/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'lcd+pot+dalgic+start+stop' 
+ * Target:  'lcd+pot+dalgic+start+stop' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
